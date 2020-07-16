569bf3e6023f7533de5474b493e974b79eb4b2a3
jbt <jbt@409_INS>
2020-07-16 18:48:46 +0300

    aaa

---
+++ Lesson06 Java Language/src/a/loops/Demo1While.java
@@ -0,0 +1,19 @@
+package a.loops;
+
+public class Demo1While {
+
+	public static void main(String[] args) {
+		
+		System.out.println("before loop");
+		
+		int counter = 0;
+		
+		while(counter < 5) {
+			System.out.println("*** Hello");
+			counter++;
+		}
+
+		System.out.println("after loop");
+	}
+
+}
---
+++ Lesson06 Java Language/src/a/loops/Demo2For.java
@@ -0,0 +1,19 @@
+package a.loops;
+
+public class Demo2For {
+
+	public static void main(String[] args) {
+
+		System.out.println("before loop");
+
+//		int counter = 0;
+
+		for (int counter = 0; counter < 5; counter++) {
+			System.out.println("*** Hello");
+//			counter++;
+		}
+
+		System.out.println("after loop");
+	}
+
+}

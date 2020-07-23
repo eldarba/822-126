6e20fe06c0528e81aa7ddf343e7b3838664f23fe
jbt <jbt@54914_>
2020-07-23 23:18:50 +0300

    aaa

---
+++ Lesson08 syntx and flow controll/src/org/eldar/java/Debug.java
@@ -0,0 +1,14 @@
+package org.eldar.java;
+
+public class Debug {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 1;
+		int z = 1;
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+}

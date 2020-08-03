2fef6f1629dc1840fda6a6603ba4a232c8a99cb0
jbt <jbt@409_INS>
2020-08-03 20:40:03 +0300

    aaa

---
+++ Lesson 11 arrays/src/a/homework/Tar1.java
@@ -0,0 +1,28 @@
+package a.homework;
+
+import java.util.Arrays;
+
+public class Tar1 {
+
+	public static void main(String[] args) {
+
+		int sum = 0; // define a variable for the numbers sum
+		System.out.println(sum);
+
+		int[] arr = new int[10]; // create an array
+		System.out.println(Arrays.toString(arr)); // print
+
+		// populate array with random numbers
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) (Math.random() * 101);
+			sum += arr[i]; // add value of current element to sum
+		}
+
+		System.out.println(Arrays.toString(arr)); // print
+		System.out.println("sum: " + sum);
+
+		double avg = (double) sum / arr.length;
+		System.out.println("average: " + avg);
+	}
+
+}

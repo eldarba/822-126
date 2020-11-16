3e3b6d0cdd4711e342723efd4cfe9c2dd60d0f78
Eldar <eldarba@gmail.com>
2020-11-16 21:30:41 +0200

    aaa

--- lesson-36-lombok/src/main/java/core/app/AppConfig.java
+++ lesson-36-lombok/src/main/java/core/app/AppConfig.java
@@ -4,13 +4,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import core.app.beans.MyBean;
+
 @Configuration
 @ComponentScan
 public class AppConfig {
 
 	public static void main(String[] args) {
 		try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class)) {
-
+			System.out.println("nothing");
+			ctx.getBean(MyBean.class);
 		}
 
 	}
--- lesson-36-lombok/src/main/java/core/app/beans/MyBean.java
+++ lesson-36-lombok/src/main/java/core/app/beans/MyBean.java
@@ -1,12 +1,28 @@
 package core.app.beans;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyBean {
+@Lazy
+public class MyBean implements BeanNameAware, BeanFactoryAware {
 
 	{
 		System.out.println(">>> MyBean CTOR");
+	}
+
+	@Override
+	public void setBeanName(String name) {
+		System.out.println(">>> MyBean aware of it's name: " + name);
+	}
+
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		System.out.println(">>> MyBean aware of it's factory: " + beanFactory);
 	}
 
 }

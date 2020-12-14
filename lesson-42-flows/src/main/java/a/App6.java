9b7cdb685be953cf80b7c8d1522fb41d6edededc
Eldar <eldarba@gmail.com>
2020-12-14 18:38:46 +0200

    111

--- lesson-42-flows/src/main/java/a/App6.java
+++ lesson-42-flows/src/main/java/a/App6.java
@@ -26,7 +26,42 @@ public class App6 {
 			session.getTransaction().begin();
 			// =====================================
 			{
+//				// create the coupons
+//				Coupon coupon1 = new Coupon("aaa");
+//				Coupon coupon2 = new Coupon("bbb");
+//				Coupon coupon3 = new Coupon("ccc");
+//				// create the customers
+//				Customer customer1 = new Customer("Dan", "dan@email");
+//				Customer customer2 = new Customer("Ariela", "ariela@email");
+//				// add the customers to the coupons
+//				coupon1.addCustomer(customer1);
+//				coupon1.addCustomer(customer2);
+//				coupon2.addCustomer(customer2);
+//
+//				// save all
+//				session.persist(coupon1);
+//				session.persist(coupon2);
+//				session.persist(coupon3);
+			}
+			{
+//				Customer customer = session.find(Customer.class, 2L);
+//				System.out.println(customer);
+//				if (customer != null) {
+//					System.out.println(customer.getCoupons());
+//				}
+			}
+			{
+//				Coupon coupon = session.find(Coupon.class, 1L);
+//				System.out.println(coupon);
+//				if (coupon != null) {
+//					System.out.println(coupon.getCustomers());
+//				}
+			}
 
+			{
+				Coupon c = new Coupon();
+				c.setId(1L);
+				session.remove(c);
 			}
 			// =====================================
 			session.getTransaction().commit();

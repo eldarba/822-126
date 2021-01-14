784d2d58464297ac78145e68d7ea63aa81b1b2e4
Eldar <eldarba@gmail.com>
2021-01-14 21:39:05 +0200

    aaa

--- lesson-47-spring-rest/src/main/java/app/core/controllers/EmployeeController.java
+++ lesson-47-spring-rest/src/main/java/app/core/controllers/EmployeeController.java
@@ -1,17 +1,25 @@
 package app.core.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.core.entities.Employee;
 import app.core.repositories.EmployeeRepository;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(path = "/api")
 public class EmployeeController {
 
 	private EmployeeRepository repo;
@@ -21,16 +29,50 @@ public class EmployeeController {
 		this.repo = repo;
 	}
 
+//	// http://localhost:8080/api/employees/xml
+//	@RequestMapping(path = "/employees/xml", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+//	public List<Employee> getAllEmployeesXML() {
+//		return repo.findAll();
+//	}
+
+	// http://localhost:8080/api/employees/json
+	@RequestMapping(path = "/employees/json", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Employee> getAllEmployeesJSON() {
+		return repo.findAll();
+	}
+
 	// http://localhost:8080/api/employees
-	@RequestMapping(path = "/employees", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
+	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return repo.findAll();
 	}
 
-	// http://localhost:8080/api/employees2
-	@RequestMapping(path = "/employees2", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Employee> getAllEmployees2() {
-		return repo.findAll();
+	// http://localhost:8080/api/employees/101
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<?> getOneEmployee(@PathVariable Long id) {
+		try {
+			Optional<Employee> opt = repo.findById(id);
+			if (opt.isPresent()) {
+				Employee employee = opt.get();
+				return ResponseEntity.ok(employee);
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).body("employee with id " + id + " not found");
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("get employee failed");
+		}
+
+	}
+
+	// http://localhost:8080/api/employees/one?id=101
+	@GetMapping("/employees/one")
+	public Employee getOneEmployeeByParam(@RequestParam Long id) {
+		return repo.getOne(id);
+	}
+
+	@PostMapping("/employees/add")
+	public Employee addEmployee(@RequestBody Employee employee) {
+		return repo.save(employee);
 	}
 
 }

841a48317ab36528e708bf3fc8033d7592397628
Eldar <eldarba@gmail.com>
2021-01-18 18:58:05 +0200

    aaa

---
+++ lesson-48-rest/src/main/java/app/core/Lesson48RestApplication.java
@@ -0,0 +1,13 @@
+package app.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson48RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson48RestApplication.class, args);
+	}
+
+}
---
+++ lesson-48-rest/src/main/java/app/core/controllers/MyController.java
@@ -0,0 +1,61 @@
+package app.core.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MyController {
+
+	private List<String> list = new ArrayList<String>();
+
+	// CREATE - add a representation to the server
+	@PostMapping("/hello")
+	public String hello1(@RequestBody String str) {
+		list.add(str);
+		System.out.println(list);
+		return "hello1 - post";
+	}
+
+	// READ - get a representation from the server
+	@GetMapping("/hello/{index}")
+	public String hello2(@PathVariable int index) {
+		String str = null;
+		if (index < list.size()) {
+			str = list.get(index);
+		}
+		return "hello2 - get: " + str;
+	}
+
+	@GetMapping("/hello")
+	public List<String> hello2All() {
+		return list;
+	}
+
+	// UPDATE - update a representation on the server
+	@PutMapping("/hello")
+	public String hello3(@RequestBody String newVal, @RequestParam int index) {
+		if (index < list.size()) {
+			list.set(index, newVal);
+			return "hello3 - updated";
+		} else {
+			return "hello3 - NOT updated";
+		}
+	}
+
+	// DELETE
+	@DeleteMapping("/hello")
+	public String hello4() {
+		return "hello4 - delete";
+	}
+}

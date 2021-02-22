0dc21ffafee0d2c52ebb859ba9935f1a2f960086
Eldar <eldarba@gmail.com>
2021-02-22 21:37:06 +0200

    aaa

---
+++ web-app1-noSession/src/main/java/app/core/filters/LoginFilter.java
@@ -0,0 +1,21 @@
+package app.core.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+// next time
+public class LoginFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
